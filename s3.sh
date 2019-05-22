15f35e2a9fa9df9c6ef06b64bce38f64583b82c1
wikiselev <vk6@sanger.ac.uk>
2019-05-22 21:42:46 +0100

    update S3 and authors

--- s3.sh
+++ s3.sh
@@ -1,6 +1,4 @@
-s3cmd -r put website s3://scrnaseq-course/
-s3cmd setacl --acl-public s3://scrnaseq-course
-s3cmd setacl --acl-public -r s3://scrnaseq-course
+s3cmd put setacl --acl-public --recursive website s3://scrnaseq-course/
 s3cmd modify --mime-type text/html s3://scrnaseq-course/website/*.html
 s3cmd modify --mime-type text/css s3://scrnaseq-course/website/*.css
 s3cmd modify --mime-type text/css s3://scrnaseq-course/website/libs/gitbook-2.6.7/css/*.css
